7d39c1e58ea7b60de3eec1cea3e49ebc5e025e55
danielvanc <email@danielvanc.com>
2020-03-24 12:46:00 +0000

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Index = () => {
+  return (
+    <>
+      <h1>EMF CI/CD Workshop</h1>
+      <p>This is the project for our workshop on Gatsby Cloud and GitHub Actions</p>
+      <p>Use this project for the workshop</p>
+    </>
+  );
+};
+
+export default Index;
