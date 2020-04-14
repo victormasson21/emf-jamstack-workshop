2333b225352e217399061cf0357a7e661c95ad63
Victor Masson <victormasson21@gmail.com>
2020-04-14 14:36:45 +0100

    First change to the project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Index = () => {
   return (
     <>
       <h1>EMF CI/CD Workshop</h1>
-      <p>This is the project for our workshop on Gatsby Cloud and GitHub Actions</p>
+      <p>
+        This is the project for our workshop on Gatsby Cloud and GitHub Actions
+      </p>
       <p>Use this project for the workshop</p>
+      <p>I've just made a change</p>
     </>
   );
 };
